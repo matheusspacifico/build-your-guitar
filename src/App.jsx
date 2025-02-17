456c38846206d28d2b59782a82765691a69454c2
matheusspacifico <matheuspacifico44@gmail.com>
2025-02-17 11:11:22 -0300

    feat: add handler functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
+import { useState } from 'react'
 import './App.css'
 import Dropdown from './components/Dropdown'
 import Textinput from './components/Textinput'
 
 function App() {
+  const numCordasDisponiveis = [6, 7, 8]
+  const formatosDisponiveis = ["Stratocaster", "Telecaster", "Les Paul"]
+
+  const [nome, setNome] = useState("John Guitarra")
+  const [numCordas, setNumCordas] = useState(numCordasDisponiveis[0])
+  const [formato, setFormato] = useState(formatosDisponiveis[6])
+
+  const [imagem, setImagem] = useState(null)
+
+  function handleName(e) {
+    setNome(e.target.value)
+  }
+
+  function handleNumCordas(e) {
+    setNumCordas(e.target.value)
+  }
+
+  function handleFormato(e) {
+    setFormato(e.target.value)
+  }
 
   return (
-    <form>
-      <Textinput name="nome" label="Digite o nome da sua guitarra:" placeholder="John Guitarra..."></Textinput>
-      <Dropdown name="cordas" label="Escolha o número de cordas:" items={[6, 7, 8]}></Dropdown>
-    </form>
+    <div>
+      <form>
+        <Textinput  name="nome" 
+                    label="Digite o nome da sua guitarra:" 
+                    placeholder="John Guitarra..."
+                    onChange={handleName}></Textinput>
+                    
+        <Dropdown   name="cordas" 
+                    label="Escolha o número de cordas:" 
+                    items={numCordasDisponiveis}
+                    onChange={handleNumCordas}></Dropdown>
+
+        <Dropdown   name="formato" 
+                    label="Escolha o formato da guitarra" 
+                    items={formatosDisponiveis}
+                    onChange={handleFormato}></Dropdown>
+      </form>
+    </div>
   )
 }
 
